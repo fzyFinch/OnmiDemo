5a1dc0c2b6887d0e25dac9239ee66f1cfeb38a9b
fangzhiyuan <fangzhiyuan@bongmi.com>
2024-08-01 10:21:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/omnidroid_fzy/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.omnidroid_fzy
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Copyright (c) 2024, Lollitech
+ * All rights reserved
+ * Author: fangzhiyuan@lollitech.com
+ */
+class MainViewModel: ViewModel() {
+}
